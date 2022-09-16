53c164b7005a7f054f269e9a2939bfcfc9032358
alexandre <alexandre.pronce@gmail.com>
2022-09-16 12:44:46 +0200

    ex1

--- S1/part1/src/App.js
+++ S1/part1/src/App.js
@@ -1,31 +1,25 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by moi
-    </div>
-  )
-}
-
+import Header from "./Components/Header/Header"
+import Content from "./Components/Content/Content"
+import Total from "./Total/Total"
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Footer/>
+      <Header course ={course}/>
+      <Content part={part1} exercises = {exercises1}/>
+      <Content part={part2} exercises = {exercises3}/>
+      <Content part={part3} exercises = {exercises3}/>
+      <Total exercises1 ={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
+
+
 export default App;
