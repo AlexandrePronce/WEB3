7830eb7331e03450249272a358ad313507e29dac
alexandre <alexandre.pronce@gmail.com>
2022-10-06 16:24:15 +0200

    stats

--- S3/ex/src/Components/Statistics/Statistics.jsx
+++ S3/ex/src/Components/Statistics/Statistics.jsx
@@ -10,6 +10,9 @@ const Statistics = ({good,bad,neutral}) => {
       <p>good :{good}</p>
       <p>neutral :{neutral}</p>
       <p>bad :{bad}</p>
+      <p>all :{good+bad+neutral}</p>
+      <p>average :{(good-bad)/(good+neutral+bad)}</p>
+      <p>positive :{(good /(good+neutral+bad))*100} %</p>
     </div>
   )
 }
